5586b3dfffee748c21b0522c351e46d5e215e9b8
davidpark-0098 <davidpark.0098@gmail.com>
2023-08-15 23:04:12 +0900

    feat: 키 추가 및 수정

--- project/assets/js/key.js
+++ project/assets/js/key.js
@@ -1,6 +1,13 @@
-/** KOPIS_공연목록 API
+/**
+ * KOPIS_공연목록 API
  * https://www.culture.go.kr/data/openapi/openapiView.do?id=422&category=A&keyword=%EA%B3%B5%EC%97%B0&searchField=all&gubun=A#/default/%EC%9A%94%EC%B2%AD%EB%A9%94%EC%8B%9C%EC%A7%80_Get
+ *
+ * KOPIS_PERFORMANCE_LIST_SERVICE_KEY
+ * 일 1000번 API 호출 제한
+ * 1. ff1e4d4d-6fdc-4642-96af-816af3919b8e
+ * 2. 957742c4-cee5-4dc8-98d2-458bfba4400b
+ * 3. 1d09d211-561c-447e-b827-df2d13b9ae26
  */
-const KOPIS_KEY = "ff1e4d4d-6fdc-4642-96af-816af3919b8e";
+const KOPIS_PERFORMANCE_LIST_SERVICE_KEY = "1d09d211-561c-447e-b827-df2d13b9ae26";
 
-export default KOPIS_KEY;
+export default KOPIS_PERFORMANCE_LIST_SERVICE_KEY;
