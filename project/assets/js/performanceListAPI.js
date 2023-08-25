0e527ceb2f614503aa550a44102a16d336072905
davidpark-0098 <davidpark.0098@gmail.com>
2023-08-25 20:35:50 +0900

    feat: API 기능 분리

---
+++ project/assets/js/performanceListAPI.js
@@ -0,0 +1,32 @@
+import KOPIS_PERFORMANCE_LIST_SERVICE_KEY from "./key.js";
+
+const performanceListAPI = async (numOfRows, currentPage) => {
+  try {
+    /**
+    // test api json
+    const response = await axios.get("http://localhost:3001/response");
+    json = response.data.body.items.item;
+    /*/
+    // api json
+    const response = await axios.get("http://api.kcisa.kr/openapi/service/rest/meta16/getkopis01", {
+      params: {
+        serviceKey: KOPIS_PERFORMANCE_LIST_SERVICE_KEY,
+        numOfRows: numOfRows,
+        pageNo: currentPage,
+      },
+      header: {
+        accept: "application/json",
+      },
+    });
+
+    // json data
+    return response.data.response.body.items.item;
+    /**/
+  } catch (e) {
+    // axios error
+    console.error(`[Error Code] ${e.code}\n[Error Message] ${e.message}\n[HTTP Status] ${e.response.status}\n[HTTP Status Text] ${e.response.statusText}`);
+    return;
+  }
+};
+
+export default performanceListAPI;
